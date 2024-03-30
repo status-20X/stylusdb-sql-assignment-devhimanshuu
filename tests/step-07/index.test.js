86077a928e6520d83b149909c155018b6d8603d1
devhimanshuu <devhimanshuu@gmail.com>
2024-03-31 02:49:02 +0530

    !

--- tests/step-07/index.test.js
+++ tests/step-07/index.test.js
@@ -92,13 +92,13 @@ test("Execute SQL Query with Multiple WHERE Clause", async () => {
 test("Execute SQL Query with Greater Than", async () => {
   const queryWithGT = "SELECT id FROM student WHERE age > 22";
   const result = await executeSELECTQuery(queryWithGT);
-  expect(result.length).toEqual(2);
+  expect(result.length).toEqual(3);
   expect(result[0]).toHaveProperty("id");
 });
 
 test("Execute SQL Query with Not Equal to", async () => {
   const queryWithGT = "SELECT name FROM student WHERE age != 25";
   const result = await executeSELECTQuery(queryWithGT);
-  expect(result.length).toEqual(2);
+  expect(result.length).toEqual(3);
   expect(result[0]).toHaveProperty("name");
 });
